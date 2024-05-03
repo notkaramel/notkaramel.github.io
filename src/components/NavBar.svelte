0bd4e382b6d4412a27b75587f003c47000049547
Antoine Phan <antoine.phan@mail.mcgill.ca>
2024-05-03 00:01:38 -0400

    Updated navbar

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,62 +1,136 @@
-<script>
+<script lang="ts">
   import profilepic from "$lib/images/pianocatpixelart-neonandretro.jpeg";
-  const hamburger =
-    '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>';
+  import {
+    fade, blur, fly, slide, scale, draw, crossfade
+  } from "svelte/transition";
+
+  $: hamburgerState = true;
+
+  function toggleHambuger() {
+    hamburgerState = !hamburgerState;
+    console.log(hamburgerState);
+  }
 </script>
 
-<div class="navbar bg-base-100 font-bold">
-  <div class="navbar-start">
-    <!-- The small screen button -->
-    <div class="avatar w-12">
-      <a href="/">
-        <img class="rounded-lg border-2 border-accent" src={profilepic} alt="piano cat pixelart neon and retro" />
-      </a>
-    </div>
+<div class="navbar">
+  <div class="navbar-left">
+    <a href="/">
+      <img
+        class="navbar-pfp"
+        src={profilepic}
+        alt="piano cat pixelart neon and retro"
+      />
+    </a>
   </div>
 
-  <!-- Center part of navbar -->
-  <div class="navbar-center hidden md:flex">
-    <ul class="menu menu-horizontal px-1 items-center">
-      <li><a href="/">Homepage</a></li>
-      <li><a href="/blog"> Blog </a></li>
-      <li><a href="/projects"> Projects </a></li>
-      <li><a href="/resume"> Resume </a></li>
-      <li><a href="/gallery"> Photography </a></li>
-      <li><a href="/music"> Music </a></li>
+  <div class="navbar-center">
+    <!-- Center part of navbar -->
+    <ul>
+      <a href="/blog">
+        <li>Blog</li>
+      </a>
+      <a href="/projects">
+        <li>Projects</li>
+      </a>
+      <a href="/resume">
+        <li>Resume</li>
+      </a>
+      <a href="/gallery">
+        <li>Photography</li>
+      </a>
+      <a href="/music">
+        <li>Music</li>
+      </a>
     </ul>
   </div>
-  
-  <!-- End part of navbar -->
-  <div class="navbar-end">
-    <details class="dropdown dropdown-bottom dropdown-end">
-      <summary class="m-1 btn">
-        Theme
-        <svg width="12px" height="12px" class="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
-      </summary>
-      <ul class="dropdown-content shadow-2xl bg-base-300 rounded-box w-24">
-        <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-center" aria-label="Default" value="karamel"/></li>
-        <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-center" aria-label="Dark" value="dark"/></li>
-        <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-center" aria-label="Bright" value="emerald"/></li>
-      </ul>
-    </details>
-    <details class="dropdown dropdown-end dropdown-bottom">
-      <summary class="btn btn-ghost md:hidden">
-        {@html hamburger}
-      </summary>
-      <ul
-        class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-right z-[1]"
-      >
-        <li><a href="/">Homepage</a></li>
-        <li><a href="/blog"> Blog </a></li>
-        <li><a href="/projects"> Projects </a></li>
-        <li><a href="/resume"> Resume </a></li>
-        <li><a href="/gallery"> Photography </a></li>
-        <li><a href="/music"> Music </a></li>
-      </ul>
-      <!-- End of the small screen button -->
-    </details>
+
+  <div class="navbar-right">
+    <button on:click={toggleHambuger}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        ></path>
+      </svg>
+    </button>
   </div>
 </div>
+{#if !hamburgerState}
+<!-- The svelte/transition module exports seven functions: fade, blur, fly, slide, scale, draw and crossfade. They are for use with Svelte transitions. -->
+  <div class="navbar-dropdown" transition:slide>
+    <ul>
+      <a href="/blog">
+        <li>Blog</li>
+      </a>
+      <a href="/projects">
+        <li>Projects</li>
+      </a>
+      <a href="/resume">
+        <li>Resume</li>
+      </a>
+      <a href="/gallery">
+        <li>Photography</li>
+      </a>
+      <a href="/music">
+        <li>Music</li>
+      </a>
+    </ul>
+  </div>
+{/if}
 
 <style lang="postcss">
+  * {
+    @apply transition-all ease-in-out;
+  }
+  .navbar {
+    @apply flex w-full justify-between items-center;
+    @apply h-14 md:h-12;
+  }
+
+  .navbar-left {
+    @apply pl-4 flex justify-center items-center gap-1;
+
+    .navbar-pfp {
+      @apply h-full w-12 md:w-10 md:h-fit rounded-lg;
+    }
+  }
+
+  .navbar-center {
+    @apply invisible md:visible;
+    > ul {
+      @apply flex items-center gap-2 justify-between;
+
+      > a {
+        @apply border-b-2 border-white w-32 py-1 text-center;
+        @apply hover:border-sky-400;
+        @apply active:border-sky-900;
+      }
+    }
+  }
+
+  .navbar-right {
+    @apply flex flex-col;
+    @apply md:collapse;
+    @apply pr-6;
+
+    > button > svg {
+      @apply h-8 stroke-current;
+      @apply border-2 rounded-lg border-transparent;
+      @apply active:border-sky-500;
+    }
+  }
+
+  .navbar-dropdown {
+    > ul {
+      @apply flex flex-col items-center gap-2 justify-between;
+
+      > a {
+        @apply border-b-2 border-white w-full py-1 text-center font-bold;
+        @apply hover:border-sky-400;
+        @apply active:border-sky-900;
+      }
+    }
+  }
 </style>
