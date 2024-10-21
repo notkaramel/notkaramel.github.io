6700b9dabcad0285c8001fe80e6967b860c7463e
Antoine Phan <antoine.phan@mail.mcgill.ca>
2024-10-20 20:17:39 -0400

    Mini cleanup

--- src/routes/api/+server.js
+++
@@ -1,8 +0,0 @@
-// /api endpoint
-
-/** @type {import('./$types').RequestHandler} */
-export async function GET() {
-    const myBlob = "hello";
-    const myOptions = { status: 200, statusText: "SuperSmashingGreat!" };
-    return new Response(myBlob, myOptions);
-};
--- src/routes/api/coffee/+server.js
+++
@@ -1,6 +0,0 @@
-/*--- /api/coffee ---*/
-
-/** @type {import('./$types').RequestHandler} */
-export async function GET() {
-    return new Response("I want tea actually\n", { status: 418 });
-};
