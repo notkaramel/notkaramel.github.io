d1474b951f5260d5f4e560403b3b003aa4b1e784
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-06-29 18:36:40 -0400

    Progress for blogs, color schemes, QOL overall
    Resolves #6 #7

---
+++ src/lib/components/Progress.svelte
@@ -0,0 +1,57 @@
+<script>
+  let {
+    checkpoints = [
+      { label: "Ideation & design", done: true },
+      { label: "Figuring out the layout", done: true },
+      { label: "Implement the core functionality", done: false },
+      { label: "Populate content", done: false },
+    ],
+  } = $props();
+</script>
+
+<ul class="timeline timeline-compact timeline-vertical">
+  {#each checkpoints as checkpoint, i}
+    <li>
+      {#if i > 0}
+        <hr class={checkpoint.done ? "bg-success" : "bg-error"} />
+      {/if}
+
+      {#if checkpoint.done}
+        <div class="timeline-start timeline-box text-success-content">
+          {checkpoint.label}
+        </div>
+        <div class="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            class="fill-success size-4"
+            ><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+            <path
+              d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"
+            />
+          </svg>
+        </div>
+      {:else}
+        <div class="timeline-start timeline-box text-error-content">
+          {checkpoint.label}
+        </div>
+
+        <div class="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            class="fill-error size-4"
+            ><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+            <path
+              d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
+            />
+          </svg>
+        </div>
+      {/if}
+
+      {#if i < checkpoints.length - 1}
+        <hr class={checkpoint.done ? "bg-success" : "bg-error"} />
+      {/if}
+    </li>
+  {/each}
+</ul>
--- src/lib/components/ResumeSection.svelte
+++ src/lib/components/ResumeSection.svelte
@@ -3,8 +3,12 @@
 </script>
 
 <div class="mb-12">
-  <div class="divider divider-middle divider-primary mb-8 prose-base"><h2>{title}</h2></div>
-  <div class="details prose-base w-full prose-ul:list-disc prose-h2:py-0 prose-h3:font-black **:my-1">
+  <div class="divider divider-middle divider-primary mb-8 prose-base md:prose-lg">
+    <h2 class="text-primary-content">{title}</h2>
+  </div>
+  <div
+    class="details prose-sm md:prose-base w-full prose-ul:list-disc prose-h2:py-0 prose-h3:font-black **:my-2 md:**:my-1"
+  >
     {@render children()}
   </div>
 </div>
