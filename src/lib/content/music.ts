836081c7da8c632794924d0ec334098205023317
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-07-20 09:38:52 -0400

    Update music

--- src/lib/content/music.ts
+++ src/lib/content/music.ts
@@ -5,15 +5,15 @@ type Transcript = {
 
 export const transcripts: Transcript[] = [
   {
-    title: "Closer – Oh My Girl",
+    title: "Closer - Oh My Girl",
     url: "https://musescore.com/user/29658671/scores/7340753",
   },
   {
-    title: "Fallin' Light – GFRIEND",
+    title: "Fallin' Light - GFRIEND",
     url: "https://musescore.com/user/29658671/scores/9363754",
   },
   {
-    title: "어린이름 (The Little Name) – ADORA (Piano Cover)",
+    title: "어린이름 (The Little Name) - ADORA",
     url: "https://musescore.com/user/29658671/scores/9208821",
   },
 ];
