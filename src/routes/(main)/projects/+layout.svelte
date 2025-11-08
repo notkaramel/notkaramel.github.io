5a727c59e2f2922254155affda89e0fb2f228178
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-11-08 15:53:20 -0500

    Major project update

--- src/routes/(main)/projects/+layout.svelte
+++ src/routes/(main)/projects/+layout.svelte
@@ -1,18 +1,19 @@
 <script>
-  import SearchEngineOp from '$lib/components/SearchEngineOp.svelte';
+  import SearchEngineOp from "$lib/components/SearchEngineOp.svelte";
 
   let { children } = $props();
 </script>
 
 <SearchEngineOp
-title="Project Gallery | Antoine @notkaramel"
-description="A collection of all technical projects I have created and/or participated in. Take a look!"
-canonical="https://antoinephan.com/projects"
+  title="Project Gallery | Antoine @notkaramel"
+  description="A collection of all technical projects I have created and/or participated in. Take a look!"
+  canonical="https://antoinephan.com/projects"
 />
+
 <div class="flex w-full min-h-screen my-8 justify-between">
   <div class="@container bg-base-100 rounded-box flex-5 mx-4 text-base-content">
     <div class="m-8">
-        {@render children()}
+      {@render children()}
     </div>
   </div>
 </div>
--- src/routes/(main)/projects/+page.svelte
+++ src/routes/(main)/projects/+page.svelte
@@ -3,12 +3,12 @@
   import { projects } from "$lib/content/projects";
 </script>
 
-<!-- Blog content are in the right side panel only -->
+<section class="flex flex-col items-center gap-6">
+  <h1 class="text-2xl text-center">I build all kinds of project!</h1>
 
-<h1 class="text-2xl text-center">I build all kinds of project!</h1>
-
-<div class="flex flex-wrap items-center justify-center gap-4">
-  {#each projects as project}
-   <ProjectCard {project}/>
-  {/each}
-</div>
+  <div class="flex w-full flex-wrap justify-center gap-6">
+    {#each projects as project}
+      <ProjectCard {project}/>
+    {/each}
+  </div>
+</section>
