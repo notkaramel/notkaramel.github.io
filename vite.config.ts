7f19da2c4363611d05bed81cd370a43010f9c6fd
Antoine Phan <hoangtuan11102@gmail.com>
2025-12-29 01:30:46 -0500

    Done with blogs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    {
+      name: "watch-blogs",
+      configureServer(server) {
+        const blogsDir = path.resolve(process.cwd(), "blogs");
+        server.watcher.add(blogsDir);
+      },
+    },
+  ],
 });
